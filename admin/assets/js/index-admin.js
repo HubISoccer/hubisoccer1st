ab25172fa3d2340163499e6d09fb234909abbc2e
HubISoccer <hubisoccer@gmail.com>
2026-02-27 07:35:54 +0100

    Delete index-admin.js

--- admin/assets/js/index-admin.js
+++
@@ -1,7 +0,0 @@
-// Déconnexion (utilisé sur toutes les pages admin)
-document.getElementById('logoutAdmin')?.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (confirm('Déconnexion ?')) {
-        window.location.href = '../../index.html';
-    }
-});
