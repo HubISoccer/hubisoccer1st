e02d02290991070455279f53ec3379da07add189
HubISoccer <hubisoccer@gmail.com>
2026-02-28 14:59:03 +0100

    Update acceuil-admin.js

--- admin/assets/js/acceuil-admin.js
+++ admin/assets/js/acceuil-admin.js
@@ -1,4 +1,4 @@
-// ===== INITIALISATION SUPABASE =====
+// ===== INITIALISATION FINAL SUPABASE =====
 const supabaseUrl = 'https://wxlpcflanihqwumjwpjs.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bHBjZmxhbmlocXd1bWp3cGpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNzcwNzAsImV4cCI6MjA4Nzg1MzA3MH0.i1ZW-9MzSaeOKizKjaaq6mhtl7X23LsVpkkohc_p6Fw';
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
