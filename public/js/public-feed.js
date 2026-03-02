e585e2facf0d47954851f06bfa1cc741087e9003
HubISoccer <hubisoccer@gmail.com>
2026-03-02 09:41:10 +0100

    Delete public-feed.js

--- public/js/public-feed.js
+++
@@ -1,8 +0,0 @@
-// ===== TEST DE BASE =====
-console.log("✅ public-feed.js est chargé !");
-const feed = document.getElementById('publicPostsFeed');
-if (feed) {
-    feed.innerHTML = '<p style="color:blue; font-size:1.2rem;">✅ Le fichier JavaScript fonctionne.</p>';
-} else {
-    console.error("❌ Élément 'publicPostsFeed' introuvable dans le HTML");
-}
