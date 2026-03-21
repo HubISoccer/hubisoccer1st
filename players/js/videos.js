062396c93705967749ded2c992ab11a26c0edc71
HubISoccer <hubisoccer@gmail.com>
2026-03-21 17:24:18 +0100

    Update videos.js

--- players/js/videos.js
+++ players/js/videos.js
@@ -1,4 +1,4 @@
-// ===== CONFIGURATION SUPABASE =====
+// ===== CONFIGURATION SUPABASE Vidéos Playes=====
 const SUPABASE_URL = 'https://wxlpcflanihqwumjwpjs.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4bHBjZmxhbmlocXd1bWp3cGpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIyNzcwNzAsImV4cCI6MjA4Nzg1MzA3MH0.i1ZW-9MzSaeOKizKjaaq6mhtl7X23LsVpkkohc_p6Fw';
 const supabasePlayersSpacePrive = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
